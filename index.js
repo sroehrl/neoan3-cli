91376889dd046fff17a691736bbf86e1c0a52cbf
sroehrl <sroehrl@eagleproductivity.com>
2018-12-21 15:36:39 -0500

    alpha testing file & folder creation

--- index.js
+++ index.js
@@ -3,25 +3,48 @@
 const program = require('commander');
 const fs = require('fs');
 let dir ='./';
-function versionJson(name){
-    return {"version":"0.0.1","name":name};
-}
+
 program
     .version('0.0.1','-v, --version')
     .option('-h, --help','option description')
     .option('-c, --component <componentname>', 'create component')
-
+    .arguments('<cmd> [name]')
+    .action(executer)
     .parse(process.argv);
-if(program.component){
-    let folder = dir+'component/'+program.component;
+function executer(cmd,name){
+    if(typeof cmd !== 'undefined'){
+        switch(cmd){
+            case 'component':
+                if(typeof name === 'undefined'){
+                    error('missing component name');
+                } else {
+                    newComponent(name)
+                }
+                break;
+            default: error();
+        }
+
+
+    }
+}
+
+function versionJson(name){
+    let json = {"version":"0.0.1","name":name};
+    return JSON.stringify(json);
+}
+function error(er){
+    console.log(er||'unknown command');
+    process.exit(1);
+}
+function newComponent(name){
+    let folder = dir+'component/'+name;
     if (!fs.existsSync(folder)){
         fs.mkdirSync(folder);
-        fs.appendFile(dir+'component/'+program.component+'/version.json',versionJson(program.component),function(err){
+        fs.appendFile(dir+'component/'+name+'/version.json',versionJson(name),function(err){
             if (err) throw err;
-            console.log('created component %s',program.component);
+            console.log('created component %s',name);
         });
     } else {
-        console.log('Component %s already exists',program.component);
+        console.log('Component %s already exists',name);
     }
-
 }
