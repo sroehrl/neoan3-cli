632d0397fe444c08e49a97e5b472f4eb6cfd4975
sroehrl <sroehrl@eagleproductivity.com>
2019-05-17 15:00:49 -0400

    improving file creation

--- concr.js
+++ concr.js
@@ -35,7 +35,10 @@ let concr = {
             case 'app':
                 res = 'newApp';
                 break;
-            default: this.error('Unknown type %s',type);
+            case 'frame':
+                res = 'newFrame';
+                break;
+            default: this.error('Unknown type '+type);
                 break;
 
         }
@@ -48,6 +51,15 @@ let concr = {
             console.log(err ? 'Could not download':msg)
         })
     },
+    newFrame:function(name){
+        fs.stat('./frame',function(err, stats){
+            if(err){
+                concr.error('Hey Stormtrooper, this is not the directory you are looking for. Navigate to the root of a neoan3 app!')
+            } else {
+                fileCreator.frame(name);
+            }
+        });
+    },
 
     newComponent:function(name){
             // in a neoan3 app?
@@ -55,34 +67,36 @@ let concr = {
                if(err){
                    concr.error('Dude, I don\'t seem to be running on the root of a neoan3 app. Are you even in the right directory?')
                } else {
+                   let frames = fs.readdirSync('./frame');
+
                    let questions = [
-                       {name:'purpose',type:'list',choices:['Route component','API endpoint','Custom Element'],message:'This component mainly serves as:'},
-                       {name:'frame',type:'input',message:'Which frame are you using?'}
+                       {name:'purpose',type:'list',choices:['Route component','API endpoint','Custom Element'],message:'This component mainly serves as:'}
                    ];
                    inquirer.prompt(questions).then(function(answer){
                        let asIdentifier;
                        switch (answer.purpose) {
                            case 'Route component':
                                asIdentifier = 'route';
+                               fileCreator.component(name,asIdentifier);
                                break;
                            case 'API endpoint':
-                               inquirer.prompt([]);
                                asIdentifier = 'api';
+                               if(frames.length<1){
+                                   this.error('I was gonna ask you which of your frames you want to use. But I find nothing, nada ...');
+                               }
+                               inquirer.prompt([{name:'frame',type:'list',choices:frames, message:'Which frame are you using?'}]).then(function(res){
+                                   fileCreator.component(name,asIdentifier,res.frame);
+                               });
+                               break;
+                           default:
+                               fileCreator.component(name,'custom');
                                break;
                        }
-                       fileCreator.component(name,asIdentifier,answer.frame);
+
                    });
                }
             });
 
-
-
-            /*process.exit(1);
-            fs.mkdirSync(folder);
-            fs.appendFile(dir+'component/'+name+'/version.json',versionJson(name),function(err){
-                if (err) throw err;
-                console.log('created component %s',name);
-            });*/
 
     },
     error:function(er){
