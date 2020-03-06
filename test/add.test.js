0be2f0e7f1072514f5ee8ada9bd9eef5708f361c
neoan <stefan.roehrl@gmail.com>
2020-03-06 17:46:18 -0500

    Add tests for actions/add

--- test/add.test.js
+++ test/add.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const add = require('../actions/add');
 const mock = require('mock-fs');
+const cp = require('child_process');
 
 const mockComposerJson = {
     repositories:[
@@ -11,6 +12,16 @@ const mockComposerJson = {
     ],
     require:{
         "sroehrl/neoan3Pwa":"dev-master"
+    },
+    extra:{
+        "installer-paths":{
+            "./frame/{$name}": [
+            ],
+            "./model/{$name}": [
+                "neoan3-model/index"
+            ],
+            "./component/{$name}": []
+        }
     }
 };
 
@@ -27,9 +38,34 @@ describe('Add', function () {
     describe('#init - composer Json', function () {
         it('should read the same composer-file as mocked up', function () {
             add.init('mock-component:dev-master');
-            let c = add.composerJson;
-            assert.deepEqual(c, mockComposerJson);
+            assert.deepEqual(add.composerJson, mockComposerJson);
+            assert.equal(add.version, 'dev-master')
+        });
+    });
+    describe('#processInput - fail', function(){
+        it('should stop the process in order to prevent overwriting added components', function () {
+            assert.rejects(async ()=>{
+                await add.processInput('sroehrl/neoan3Pwa', 'component', 'https://github.com/sroehrl/neoan3Pwa.git')
+            });
+
         })
     });
-
+    describe('#processInput - composer package', function(){
+        it('should find the composer package', async function(){
+            this.timeout(4000);
+            process.env.MOCKCP = true;
+            await add.processInput('vast-n3/vastn3','frame');
+            add.getComposerJson();
+            assert.equal(add.composerJson.require["vast-n3/vastn3"],'dev-master');
+        })
+    });
+    describe('#processInput - custom repo', function(){
+        it('should find the github repo', async function(){
+            this.timeout(4000);
+            process.env.MOCKCP = true;
+            await add.processInput('neoan3-model/user','model', 'https://githunb.com/sroehrl/neoan3-userModel.git');
+            add.getComposerJson();
+            assert.equal(add.composerJson.require["neoan3-model/user"],'dev-master');
+        })
+    });
 });
